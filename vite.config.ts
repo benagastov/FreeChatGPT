0e9a3663f62c6797d0bf4f64ba3358b8eb2b9f92
benagastov <106045877+benagastov@users.noreply.github.com>
2023-03-14 17:36:18 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/FreeChatGPT/'
+  base: '/FreeChatGPT/', // Set base to the subpage path on GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
